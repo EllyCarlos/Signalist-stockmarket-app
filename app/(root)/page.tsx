be3659dcea24aa9d2d1ed4680eaf51bcaeb6db5d
EllyCarlos <ellycarlos7915@gmail.com>
2026-05-01 11:42:57 +0300

    implement header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button"
+
+const Home = () => {
+    return (
+        <div className="flex items-center justify-center min-h-[calc(100vh-70px)] text-gray-400">
+            Home
+        </div>
+    );
+};
+
+export default Home;
